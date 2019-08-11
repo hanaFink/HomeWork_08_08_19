53697c30a0cce691bd1c4eace9e3b43e5071bed5
hanaFink <44664936+hanaFink@users.noreply.github.com>
2019-08-11 20:44:20 +0300

    Add files via upload

---
+++ Threads/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Threads
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            WebTheads site = new WebTheads("http://www.ynet.co.il");
+           WebTheads site1 = new WebTheads("http://www.walla.co.il");
+            Thread thread = new Thread(site.DownloadString);
+           Thread thread1 = new Thread(site1.DownloadString);
+            thread.Start();
+            thread1.Start();
+
+
+
+        }
+    }
+}
---
+++ Threads/WebTheads.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
+
+namespace Threads
+{
+    class WebTheads
+    {
+
+        private string reply;
+        private string address;
+        public int Counter { get; protected set;}
+
+        public WebTheads(string address)
+        {
+            this.address = address;
+        }
+        public void DownloadString()
+        {
+            WebClient client = new WebClient();
+            reply = client.DownloadString(this.address);
+            Console.WriteLine(reply);
+            for (int i = 0; i < reply.Length; i++)
+            {
+                if (reply[i] == 'a')
+                    Counter++;
+            }
+            Console.WriteLine("number of a in this object is"+Counter);
+        }
+
+
+
+    }
+}
