030fbb24ebad3cdd9e78e4153bc14fadaa9a9495
hanaFink <zloyfrank@gmail.com>
2019-08-11 20:41:23 +0300

    Add project files.

---
+++ Asynchronic/WebAsinc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Asynchronic
+{
+    class WebAsinc
+    {
+        
+
+        private string reply;
+   
+        public int Counter { get; protected set; }
+
+        public async Task DownloadString(string address)
+        {
+            WebClient client = new WebClient();
+            reply = client.DownloadString(address);
+            Console.WriteLine(reply);
+            for (int i = 0; i < reply.Length; i++)
+            {
+                if (reply[i] == 'a')
+                    Counter++;
+            }
+            Console.WriteLine("number of a in this object is " + Counter);
+        }
+        public async Task DoWorkAsync ()
+        {
+            var t1 = DownloadString("http://www.walla.co.il");
+            var t2 = DownloadString("http://www.ynet.co.il");
+            await Task.WhenAll(t1, t2);
+            Console.WriteLine("Done");
+
+
+        }
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net;
+
+namespace HomeWork_08_08_19
+{
+
+    class Program
+    {
+        public static void DownloadString(string address)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString(address);
+
+            Console.WriteLine(reply);
+        }
+        static void Main(string[] args)
+        {
+            DownloadString("http://www.ynet.co.il");
+            DownloadString("http://www.walla.co.il");
+        }
+       
+    }
+}
